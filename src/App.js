f0bb51218ca388586dc24dd75dd9e8eae51cbd15
Steve Burton <steveburtonwebdev@gmail.com>
2019-11-03 17:04:49 +0000

    signinpage routed, form added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.js';
 import ShopPage from './pages/shop/shop.component.js';
 import Header from './components/header/header.component.js';
+import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component.js';
+
+
 
 
 function App() {
@@ -13,6 +16,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+
       </Switch>
 
     </div>
---
+++ src/pages/signin-and-signup/signin-and-signup.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './signin-and-signup.style.scss';
+import SignIn from '../../components/signin/signin.component.js';
+
+
+const SignInAndSignUpPage = () => (
+    <div className='signinandsignup'>
+       <SignIn />
+    </div>
+);
+
+export default SignInAndSignUpPage;
